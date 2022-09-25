d954d6e230415d7f094212d7703580266b1e4999
Dzikri Syafi <dzikrisyafi@gmail.com>
2022-09-25 10:55:31 +0700

    Initial commit

---
+++ migrations/1663370275561_create-table-albums.js
@@ -0,0 +1,31 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
